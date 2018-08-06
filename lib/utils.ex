3b4387e97557f061b1bbf5b9b3a2636bce4871ac
AlexB <ua3mqj@gmail.com>
2018-08-06 19:40:31 +0300

    remove comments

--- lib/utils.ex
+++ lib/utils.ex
@@ -1,33 +1,55 @@
 defmodule OberLexx.Utils do
+  require Logger
 
-  def remove_comments(in_file, out_file) do
+  def file_remove_comments(in_file, out_file) do
     {:ok, binary} = File.read(in_file)
+    # t1 = raw_binary_to_string(binary)
+    # IO.puts(t1)
+    new_binary = binary_remove_comments(binary)
+    # t2 = raw_binary_to_string(new_binary)
+    # IO.puts(t2)
+
     file = File.open!(out_file, [:write])
-    remove_comments_filter(binary, 0, file)
-  end
-  def remove_comments_filter(<<>>, _stack, file) do
+    IO.binwrite(file, new_binary)
     File.close(file)
   end
-  def remove_comments_filter(<< "(*", tail :: binary >>, stack, file) do
-    remove_comments_filter(tail, stack + 1, file)
+
+  def binary_remove_comments(in_binary) do
+    binary_remove_comments(in_binary, 0, <<>>)
   end
-  def remove_comments_filter(<< "*)", tail :: binary >>, stack, file) do
-    remove_comments_filter(tail, stack - 1, file)
+  def binary_remove_comments(<<>>, _stack, out_binary) do
+    out_binary
   end
-  def remove_comments_filter(<< "\n", tail :: binary >>, stack, file) do
-    IO.binwrite(file, << "\n" >>)
-    remove_comments_filter(tail, stack, file)
+  def binary_remove_comments(<< "(*", tail :: binary >>, stack, out_binary) do
+    binary_remove_comments(tail, stack + 1, out_binary)
   end
-  def remove_comments_filter(<< "\r", tail :: binary >>, stack, file) do
-    IO.binwrite(file, << "\r" >>)
-    remove_comments_filter(tail, stack, file)
+  def binary_remove_comments(<< "*)", tail :: binary >>, stack, out_binary) do
+    binary_remove_comments(tail, stack - 1, out_binary)
   end
-  def remove_comments_filter(<< header :: size(8), tail :: binary >>, 0, file) do
-    IO.binwrite(file, << header :: size(8) >>)
-    remove_comments_filter(tail, 0, file)
+  def binary_remove_comments(<< "\n", tail :: binary >>, stack, out_binary) do
+    binary_remove_comments(tail, stack, out_binary <> << "\n" >>)
   end
-  def remove_comments_filter(<< _header :: size(8), tail :: binary >>, stack, file) do
-    remove_comments_filter(tail, stack, file)
+  def binary_remove_comments(<< "\r", tail :: binary >>, stack, out_binary) do
+    binary_remove_comments(tail, stack, out_binary <> << "\r" >>)
+  end
+  def binary_remove_comments(<< header :: size(8), tail :: binary >>, 0, out_binary) do
+    binary_remove_comments(tail, 0, out_binary <> << header :: size(8) >>)
+  end
+  def binary_remove_comments(<< _header :: size(8), tail :: binary >>, stack, out_binary) do
+    binary_remove_comments(tail, stack, out_binary)
+  end
+
+  def raw_binary_to_string(raw) do
+    codepoints = String.codepoints(raw)  
+    Enum.reduce(codepoints, fn(w, result) ->  
+                                cond do 
+                                  String.valid?(w) -> 
+                                      result <> w 
+                                  true ->
+                                      << parsed :: 8>> = w 
+                                      result <>   << parsed :: utf8 >>
+                                end
+                            end)
   end
 
 end
--- test/removecomments_test.exs
+++ test/removecomments_test.exs
@@ -6,11 +6,37 @@ defmodule RemoveCommentsTest do
   # mix test --only removecomments
   @tag removecomments: true  
 
-  test "removecomments" do
-    in_file_name = "./priv/mods/examples/Laser2Demo.Mod"
-    out_file_name = "./priv/mods/examples/Laser2Demo_clean.Mod"
+  # test "removecomments file" do
+  #   in_file_name = "./priv/mods/examples/BrightLetters.Mod"
+  #   out_file_name = "./priv/mods/examples/BrightLetters_clean.Mod"
 
-    OberLexx.Utils.remove_comments(in_file_name, out_file_name)
+  #   OberLexx.Utils.file_remove_comments(in_file_name, out_file_name)
+  # end
+
+  test "removecomments binary" do
+    charlist = '''
+        MODULE ASCII;(* some comment *)
+        IMPORT system;
+
+        END ASCII.
+    '''
+    instr = to_string(charlist)
+
+    # Logger.debug "#{inspect instr}"
+
+    charlist_clean = '''
+        MODULE ASCII;
+        IMPORT system;
+
+        END ASCII.
+    '''
+    truestr = to_string(charlist_clean)
+
+    new_str = OberLexx.Utils.binary_remove_comments(instr)
+
+    # Logger.debug "#{inspect new_str}"
+
+    assert new_str === truestr
   end
 
 end
